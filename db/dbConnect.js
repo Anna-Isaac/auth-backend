d0ac26fab5a717ed8069ce620101f2cb425a7b3b
Teanna Isaac <annaisaacdeveloper@gmail.com>
2023-12-17 00:40:12 +0000

    feat(api): implement user registration endpoint
    Added a new API endpoint for user registration. Handles POST requests to /register, storing user email and hashed password in the database. Also, implemented error handling and validation for duplicate emails.

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,8 +1,26 @@
 // external imports
+const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config()
 
+const app = express();
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 async function dbConnect() {
+
   // use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
   mongoose
     .connect(
